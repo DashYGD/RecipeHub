8017fd4f0587a63d9613a3ecd5b8196436a262ce
TaavaSyri <taava.syri@gmail.com>
2024-03-27 09:22:15 +0200

    Reseptikorttien ja kuvakkeiden suunnittelua
    Saa antaa mielipiteitä, vinkkejä, ohjeita yms. Osa noista kuvista on ylimääräisiä mut annetaan niiden vielä olla, voi poistella myöhemmin jos ei tarvi

---
+++ reseptikuvakkeet.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esimerkki</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Voit käyttää erillistä CSS-tiedostoa tyyleille -->
+</head>
+<body>
+    <div class="icon" onclick="navigate('aamupala')">
+        <img src="aamupala_icon.png" alt="Aamupala">
+        <p>Aamupala</p>
+    </div>
+
+    <div class="icon" onclick="navigate('lounas')">
+        <img src="lounas_icon.png" alt="Lounas">
+        <p>Lounas</p>
+    </div>
+
+    <div class="icon" onclick="navigate('illallinen')">
+        <img src="illallinen_icon.png" alt="illallinen">
+        <p>Illallinen</p>
+    </div>
+
+    <div class="icon" onclick="navigate('valipala')">
+        <img src="valipala_icon.png" alt="Valipala">
+        <p>Välipalat</p>
+    </div>
+
+    <div class="icon" onclick="navigate('jalkiruoka')">
+        <img src="jalkiruoka_icon" alt="jalkiruoka">
+        <p>Jälkiruokia</p>
+    </div>
+
+    <div class="icon" onclick="navigate('iltapala')">
+        <img src="iltapala_icon.png" alt="iltapala">
+        <p>Välipalat</p>
+    </div>
+
+</body>
+</html>
